c2d8bada014d311ed292ec9d1d9be2a66e797174
Brian Vincent <brainn@gmail.com>
2024-07-17 22:35:38 -0700

    wip

--- flake.nix
+++ flake.nix
@@ -30,12 +30,12 @@
             # arguments to home.nix
           }
           {
-            nixpkgs.overlays = [
-              (final: prev: {
-                gnome.gnome-shell = unstable.packages.${system}.gnome.gnome-shell;
-                gnome.mutter = unstable.packages.${system}.gnome.mutter;
-              })
-            ];
+            # nixpkgs.overlays = [
+            #   (final: prev: {
+            #     gnome.gnome-shell = unstable.packages.${system}.gnome.gnome-shell;
+            #     gnome.mutter = unstable.packages.${system}.gnome.mutter;
+            #   })
+            # ];
             # nixpkgs.overlays = [
             #   # GNOME 46: triple-buffering-v4-46
             #   (final: prev: {
@@ -52,23 +52,23 @@
             #     });
             #   })
             # ];
-            # # Enable triple buffering patch for GNOME
-            # nixpkgs.overlays = [
-            #   # GNOME 46: triple-buffering-v4-46
-            #   (final: prev: {
-            #     gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-            #       mutter = gnomePrev.mutter.overrideAttrs (old: {
-            #         src = pkgs.fetchFromGitLab {
-            #           domain = "gitlab.gnome.org";
-            #           owner = "vanvugt";
-            #           repo = "mutter";
-            #           rev = "triple-buffering-v4-46";
-            #           hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
-            #         };
-            #       });
-            #     });
-            #   })
-            # ];
+            # Enable triple buffering patch for GNOME
+            nixpkgs.overlays = [
+              # GNOME 46: triple-buffering-v4-46
+              (final: prev: {
+                gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+                  mutter = gnomePrev.mutter.overrideAttrs (old: {
+                    src = pkgs.fetchFromGitLab {
+                      domain = "gitlab.gnome.org";
+                      owner = "vanvugt";
+                      repo = "mutter";
+                      rev = "triple-buffering-v4-46";
+                      hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
+                    };
+                  });
+                });
+              })
+            ];
 
           }
         ];
