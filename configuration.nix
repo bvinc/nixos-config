a0101900703a3eae9b1eeb5eb3176fc9b4968e08
Brian Vincent <brainn@gmail.com>
2024-07-24 21:48:09 -0700

    wip

--- configuration.nix
+++ configuration.nix
@@ -96,9 +96,13 @@
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
+  environment.enableDebugInfo
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+    gdb
+    killall
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
