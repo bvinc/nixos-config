0a4aa807fa35893f19a2223ca18ef9de5d3da784
Brian Vincent <brainn@gmail.com>
2024-09-18 22:21:00 -0700

    default to gnome

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.defaultSession = "gnome";
+
   services.xserver.desktopManager.gnome = {
     enable = true;
 
