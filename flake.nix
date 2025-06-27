6d9078ef9fd9a44a7c303b1982e84bea043c611a
Brian Vincent <brainn@gmail.com>
2025-06-27 00:35:14 -0700

    use github: url

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # local-mutter = {
     #   url = "path:/home/brain/src/mutter";
