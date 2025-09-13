e004a82bb181e76b2fec7cbf03567d4f896ebd18
Brian Vincent <brainn@gmail.com>
2025-09-13 13:35:21 -0700

    update

--- modules/home-manager/nix-rs-init.nix
+++ modules/home-manager/nix-rs-init.nix
@@ -45,29 +45,40 @@
             description = "Rust dev env with fenix";
 
             inputs = {
-              nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+              nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
               fenix.url = "github:nix-community/fenix";
               fenix.inputs.nixpkgs.follows = "nixpkgs";
               flake-utils.url = "github:numtide/flake-utils";
             };
 
-            outputs = { self, nixpkgs, fenix, flake-utils, ... }:
-              flake-utils.lib.eachDefaultSystem (system:
+            outputs =
+              {
+                self,
+                nixpkgs,
+                fenix,
+                flake-utils,
+                ...
+              }:
+              flake-utils.lib.eachDefaultSystem (
+                system:
                 let
                   pkgs = import nixpkgs { inherit system; };
-                  toolchain = fenix.packages.''${system}.stable.withComponents [
+                in
+                {
+                  devShells.default = pkgs.mkShell {
+                    packages = with pkgs; [
+                      (fenix.packages.''${system}.stable.withComponents [
                         "cargo"
                         "clippy"
                         "rust-src"
                         "rustc"
                         "rustfmt"
-                  ];
-                  ra = fenix.packages.''${system}.rust-analyzer;
-                in {
-                  devShells.default = pkgs.mkShell {
-                    buildInputs = [ toolchain ra ];
+                        "rust-analyzer"
+                      ])
+                    ];
                   };
-                });
+                }
+              );
           }
           EOF
 
